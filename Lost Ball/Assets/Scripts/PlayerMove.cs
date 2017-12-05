48c197585baec350e71e0700cffd78be09758afb
MochTor <tieghi.marco@gmail.com>
2017-12-05 22:43:45 +0100

    First commit

---
+++ Lost Ball/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+	//Setting the color of local player as red as opposed to the other player which will be blue
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	void Update()
+	{
+		// checking is this command given by the local player
+		if (!isLocalPlayer)
+			return;
+
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
